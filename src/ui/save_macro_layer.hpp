41a1eb4811a83848017cada388f3e0e081d808a0
flinger-bit <xdffsxdxd@gmail.com>
2026-04-15 13:17:39 -0700

    Update save_macro_layer.hpp

--- src/ui/save_macro_layer.hpp
+++ src/ui/save_macro_layer.hpp
@@ -4,21 +4,25 @@
 
 using namespace geode::prelude;
 
-class SaveMacroLayer : public Popup<> {
+class SaveMacroLayer : public Popup {
 protected:
-    bool setup() override {
+    bool init() {
+        if (!Popup::init(300.f, 200.f))
+            return false;
+
         this->setTitle("Save Macro");
+
         return true;
     }
 
 public:
     static SaveMacroLayer* create() {
         auto ret = new SaveMacroLayer();
-        if (ret && ret->initAnchored(300.f, 200.f)) {
+        if (ret && ret->init()) {
             ret->autorelease();
             return ret;
         }
-        CC_SAFE_DELETE(ret);
+        delete ret;
         return nullptr;
     }
 };
